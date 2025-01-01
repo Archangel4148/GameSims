54f7d889b524404444b291c1c26e2e9e07e7b4c7
Simon Edmunds <sjebkc@umsystem.edu>
2025-01-01 00:45:55 -0600

    Fixing threshold management and cleaning up comments

--- War/main.py
+++ War/main.py
@@ -1,7 +1,7 @@
 from War.war_tools import WarDeck, Player, resolve_war
 
 
-def run_simulation(player_count: int):
+def run_simulation(player_count: int, round_threshold: int):
     # This simulation currently only supports 2 player games of War
     players = [Player(f"Player {i + 1}") for i in range(player_count)]
 
@@ -10,17 +10,14 @@ def run_simulation(player_count: int):
     deck.deal(players)
     dealt_hands = [[str(card) for card in player.deck] for player in players]
 
-    # print("=== Players ===")
-    # for player in players:
-    #     print(str(player))
-
+    # Initialize result variables
     card_counts = [player.card_count for player in players]
     overall_winner = None
     rounds = 0
     war_count = 0
     player_wins = [0] * len(players)
 
-    while min(card_counts) > 0 and rounds < 10000:
+    while min(card_counts) > 0 and rounds < round_threshold:
         rounds += 1
         # Each player plays a card from the top of their deck
         winning_card = None
@@ -29,8 +26,6 @@ def run_simulation(player_count: int):
             played_card = player.deck.pop()
             player.card_count -= 1
             all_plays.append(played_card)
-
-            # print(f"{player.name} plays the {played_card}")
 
             if winning_card is None:
                 winning_card = played_card
@@ -53,33 +48,28 @@ def run_simulation(player_count: int):
         war_winner.card_count += len(won_cards)
         player_wins[players.index(war_winner)] += 1
 
-        # print(f"Winner: {war_winner.name} ({war_winner.card_count})")
-
         # Update card counts for tracking
         card_counts = [player.card_count for player in players]
         if 0 in card_counts:
             overall_winner = war_winner
 
-    # print(f"\nGame ended after {rounds} rounds")
-    # print(f"War Count: {war_count}\n")
-    # print("=== Player Wins ===")
-    # for win_count, player in zip(player_wins, players):
-    #     print(f"{player.name}: {win_count}")
-    # if rounds == 100000:
-    #     print("For some reason, this game lasted at least 10,000 rounds without a winner, so I'm not simulating any more.")
-    # else:
-    #     print(f"Final Winner: {overall_winner.name}")
     return players, dealt_hands, overall_winner, war_count, player_wins, rounds
 
 
 if __name__ == "__main__":
+    # Initialize resulting variables
     current_round_count = 0
+    current_war_count = 0
+    current_player_wins = None
+    current_players = None
+    current_winner = None
+    dealt_hands = None
     game_number = 0
     round_threshold = 10000
 
     while current_round_count < round_threshold:
         current_players, dealt_hands, current_winner, current_war_count, current_player_wins, current_round_count = run_simulation(
-            2)
+            2, round_threshold)
         game_number += 1
 
     print(f"\nGame number {game_number} ended after {current_round_count} rounds")
@@ -92,6 +82,6 @@ if __name__ == "__main__":
 
     if current_round_count == round_threshold:
         print(
-            f"\nFor some reason, this game lasted at least {round_threshold} rounds without a winner, so I'm not simulating any more.")
+            f"\nEnding simulation after game exceeded the threshold of {round_threshold} rounds without a winner.")
     else:
         print(f"\nFinal Winner: {current_winner.name}")
