1487359358e092898781a1a96acfaa8409670e51
Simon Edmunds <sjedmunds04@gmail.com>
2025-01-30 11:44:10 -0600

    Started implementing beginning game logic

--- Blackjack/blackjack_tools.py
+++ Blackjack/blackjack_tools.py
@@ -1,17 +1,47 @@
 import dataclasses
+import random
 
-from playing_card_tools import Card
+from playing_card_tools import Card, Deck
 
 
 @dataclasses.dataclass
 class Player:
     name: str = "NO_NAME"
     hand: list[Card] | None = None
-    hand_value: int = 0
+    hand_values: list[int] | None = None
 
     def __post_init__(self):
         if self.hand is None:
             self.hand = []
+        if self.hand_values is None:
+            self.hand_values = []
 
     def __str__(self):
         return f"{self.name}: {[str(card) for card in self.hand]}"
+
+
+class BlackjackDeck(Deck):
+    def __init__(self):
+        super().__init__()
+
+    def deal_hands(self, players: list[Player]):
+        for _ in range(2):
+            for player in players:
+                drawn_card = self.draw_card()
+                player.hand.append(drawn_card)
+                player.hand_values.append(get_card_value(drawn_card))
+
+    def hit(self, player: Player):
+        hit_card = self.draw_card()
+        player.hand.append(hit_card)
+        player.hand_values.append(get_card_value(hit_card))
+
+
+def get_card_value(card: Card) -> int:
+    # Face cards are worth 10, everything else is face value
+    if card.value < 11:
+        return card.value
+    elif card.value == 14:
+        return 11
+    else:
+        return 10
--- Blackjack/main.py
+++ Blackjack/main.py
@@ -1,14 +1,18 @@
-from Blackjack.blackjack_tools import Player
-
+from Blackjack.blackjack_tools import Player, BlackjackDeck
 PLAYER_COUNT = 4
 
 
 def main():
+    # Create a list of players (and a dealer)
     dealer = Player(name="Dealer")
     players = [Player(f"Player {i + 1}") for i in range(PLAYER_COUNT)]
 
+    # Create a deck and deal a hand to each player
+    deck = BlackjackDeck()
+    deck.deal_hands([dealer, *players])
+
     for player in (dealer, *players):
-        print(player)
+        print(player, player.hand_values)
 
 
 if __name__ == '__main__':
